9f5cce8a8feee374b406b31d2d1baf9a85f7158b
Luca Angelini <luca.angelini85@gmail.com>
2014-09-26 14:52:12 +0200

    Reso statico metodo getIdCampionato()

--- business/classifica.php
+++ business/classifica.php
@@ -15,14 +15,11 @@ class RigaClassifica{
 $lines = file('http://imalatidelbari.netsons.org/js/fcmClassificaDati.js');
 $squadraClassifica = array();
 
-$comp = new Competizione()
-echo "IdCampionato = " . $comp->getIdCompetizione();
-
 // Ciclo attraverso l'array, si visualizzerà il sorgente come html ed i numeri di linea
 foreach($lines as $line_num => $line) {
     if (strpos($line,']=new C(') !== false){
     	$riga = explode(",", $line);        
-    	if ($riga[1] == '14'){
+    	if ($riga[1] == Competizione::getIdCampionato()){
     		$rigaClas = new RigaClassifica();
     		$rigaClas->squadra = str_replace('"',"",$riga[4]);
             $rigaClas->allenatore = str_replace('"', "", $riga[5]);
