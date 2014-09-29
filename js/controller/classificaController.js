7f912a4ecc8e77d9db8b548e39f19eb296033c09
gbottalico <giovanni.bottalico@finconsgroup.com>
2014-09-29 18:04:10 +0200

    commit

--- js/controller/classificaController.js
+++ js/controller/classificaController.js
@@ -1,5 +1,8 @@
 imdbFanta.controller('classificaCtrl', function ($scope, $http) {
+	$scope.loading = true;
+	
 	$http.get('service/classificaService.php').success(function(data) {
+		$scope.loading = false;
 	    $scope.squadre = data;
 	  });
 });
