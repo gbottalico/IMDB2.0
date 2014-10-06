538b41a9de963edd6f81c5cf7e148078fdf0c4c8
gbottalico <giovanni.bottalico@finconsgroup.com>
2014-10-06 18:05:33 +0200

    commit

--- js/interface.js
+++ js/interface.js
@@ -1,24 +1,7 @@
 function initializeMenu(){
-	initializeDimension();
 	$('#menuClassifica').click(loadPageClassifica);
 }
 
-function initializeDimension(){
-	var altezza = $('#wrapper').height();
-	var larghezza = $('#wrapper').width();
-	var dimensioni;
-	
-	if (altezza < larghezza) { 
-		$('.menu').css('width', $('#wrapper').height());
-		dimensioni = ($('#wrapper').height() * 32)/100;
-	}else{
-		$('.menu').css('width', $('#wrapper').width());
-		dimensioni = ($('#wrapper').width() * 32)/100;
-	}
-	
-	$('.menu li').css('height', dimensioni);
-	$('.menu li').css('width', dimensioni);
-}
 
 function loadPageClassifica(){
 	window.location = "classifica.html";
