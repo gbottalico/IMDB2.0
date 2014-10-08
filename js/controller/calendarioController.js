5d16ba147f73e3fb29fd438757ae5e532b96504e
gbottalico <giovanni.bottalico@finconsgroup.com>
2014-10-08 18:04:49 +0200

    commit

--- js/controller/calendarioController.js
+++ js/controller/calendarioController.js
@@ -1,15 +1,26 @@
 imdbFanta.controller('calendarioCtrl', function($scope, $http) {
 	$scope.loading = true;
-
-	$http.get('service/calendarioService.php').success(function(data) {
-		$scope.loading = false;
-		$scope.calendario = data;
-	});
-
+	
+	$scope.caricaCompetizione =  function(competizione){
+		$scope.competizioneSelected = competizione;
+		$http.get('service/calendarioService.php').success(function(data) {
+			$scope.loading = false;
+			$scope.calendario = data.filter(function(row) {
+				if (row.idCompetizione == competizione) {
+					return true
+				} else {
+					return false;
+				}
+			});
+			
+			
+		});
+	}
+	
 	$scope.visualizzaGiornata = function() {
 		$scope.loadingGiornata = true;
 		$scope.infoGiornata = $scope.calendario.filter(function(row) {
-			if (row.idGiornata == $scope.giornataSelected) {
+			if (row.idGiornata == $scope.giornataSelected && row.idCompetizione == $scope.competizioneSelected) {
 				return true
 			} else {
 				return false;
