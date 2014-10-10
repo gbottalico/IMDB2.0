a38863677a1b7b50b4ab2d968ab94329c56ac2cc
gbottalico <giovanni.bottalico@finconsgroup.com>
2014-10-10 10:15:07 +0200

    commit

--- js/controller/calendarioController.js
+++ js/controller/calendarioController.js
@@ -1,26 +1,37 @@
 imdbFanta.controller('calendarioCtrl', function($scope, $http) {
 	$scope.loading = true;
 	
-	$scope.caricaCompetizione =  function(competizione){
-		$scope.competizioneSelected = competizione;
+	$scope.initCompetizione =  function(){
 		$http.get('service/calendarioService.php').success(function(data) {
 			$scope.loading = false;
-			$scope.calendario = data.filter(function(row) {
-				if (row.idCompetizione == competizione) {
+			$scope.calendario = data;
+			$scope.competizioneSelected = $scope.calendario[0].competizione;
+			$scope.calendarioComp = $scope.calendario.filter(function(row) {
+				if (row.competizione == $scope.competizioneSelected) {
 					return true
 				} else {
 					return false;
 				}
-			});
-			
-			
+			});			
 		});
 	}
 	
-	$scope.visualizzaGiornata = function() {
+	$scope.caricaCompetizione =  function(competizione){
+		
+		$scope.competizioneSelected = competizione;
+		$scope.calendarioComp = $scope.calendario.filter(function(row) {
+			if (row.competizione == $scope.competizioneSelected) {
+				return true
+			} else {
+				return false;
+			}
+		});
+	}
+	
+	$scope.visualizzaGiornata = function(idComp) {
 		$scope.loadingGiornata = true;
 		$scope.infoGiornata = $scope.calendario.filter(function(row) {
-			if (row.idGiornata == $scope.giornataSelected && row.idCompetizione == $scope.competizioneSelected) {
+			if (row.idGiornata == idComp && row.competizione == $scope.competizioneSelected) {
 				return true
 			} else {
 				return false;
